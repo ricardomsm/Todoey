e738d1c5709c63d295b5e83fc86635cd7f1f03ad
Ricardo Magalhães <ricardomm201@gmail.com>
2018-09-09 14:51:29 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ricardo Magalhães on 09/09/18.
+//  Copyright © 2018 Ricardo Magalhães. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
